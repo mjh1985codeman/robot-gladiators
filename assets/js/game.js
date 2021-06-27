9f76069a01f570ee6e928f6bbe63a9bebf75dc51
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-06-27 14:52:26 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,77 +9,15 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-//***************************************************************************************** */
-/*This is called an 'Array' Declaration. . .the Brackets hold the "Array Elements"
-Array elements are stored in the array at specific indexes. You can think of these indexes 
-like the tabs that organize the contents of a file drawer. When a particular index is called, the value
-stored at that index is retrieved. The first index in an array is zero, and the indexes increment by 1 
-for each corresponding element. So the second element will be at index 1, the third element will be at index 2, and so on.
-So To retrieve the first element in the array, we'll use the following statement:
-
-console.log(enemyNames[0]);
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length); //<--- This can be used to find the length of an array in the console.
-
-//**************************************************************************************** */
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-
-console.log(enemyNames);
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length);
-
-for(var i = 0; i < enemyNames.length; i++) {
-  console.log(enemyNames[i]);
-  console.log(i);
-  console.log(enemyNames[i] + " is at " + i + " index");
-}
-
-/* "for loops statements" enable us to loop through the array no matter the length which can be used whenever
-we need the same operation done repetitively, so it's NOT JUST for arrays.  Here is an example: 
-
-**This is the template for 'for statements'**
-
-for([initial expression]; [condition]; [increment expression]) {
-  statement
-}
-example:
-
-for(var i = 0; i < 3; i++) {
-  console.log("apple");
-}
-
-Syntax Breakdown: the 'for' loop is a special type of statement called a 'control flow'.  The control flow
-is the order in which the computer executes statements in a JavaScript file or script, which normally runs sequentially
-from the first line to the last line.  Control flow statements such as conditional statements or 'for' statements, change 
-the control flow based on the statement's conditions. 
-*/
-
 var enemyHealth = 50;
-
 var enemyAttack = 12;
 
-/* this is another way to create a function called "function declaration":
+window.alert("Welcome to Robot Gladiators!");
 
-function fight() {
-    window.alert("Welcome to Robot Gladiators!");
-} 
-
-*/
-
-// create function. . .This method is called Function Expression.
-
-var fight = function() {
+var fight = function(enemyName) {
   // Alert Players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+
 // Confirm if player wants to FIGHT or SKIP the battle. 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -143,6 +81,9 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   
 
 
-//To execute the function "fight"
 
 //fight();
+
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
